0462f0242c40bccb296489348bb087d1422b86e4
stefawn <mail.stefanyoung@gmail.com>
2016-09-05 14:53:59 -0700

    remove map testing

--- homework/Foodstr/js/foodstr.js
+++ homework/Foodstr/js/foodstr.js
@@ -119,67 +119,67 @@ function favListFirebase() {
 
 // Map Functions
 // ------------------------------------
-function initMap() {
-	loadMap();
-}
+// function initMap() {
+// 	loadMap();
+// }
 
-function loadMap() {
-	var el = document.querySelector('#places-map');
+// function loadMap() {
+// 	var el = document.querySelector('#places-map');
 
-	var options = {
-		center: { lat: 37.798969, lng: -122.407312 },
-		zoom: 10
-	};
+// 	var options = {
+// 		center: { lat: 37.798969, lng: -122.407312 },
+// 		zoom: 10
+// 	};
 
-	map = new google.maps.Map(el, options);
+// 	map = new google.maps.Map(el, options);
 
-}
+// }
 
-function createMarkers() {
-	// clears out previous map markers
-	deleteOverlays();
+// function createMarkers() {
+// 	// clears out previous map markers
+// 	deleteOverlays();
 
-	for (i = 0; i < pinLocations.length; i++) {
-		markers[i] = new google.maps.Marker({
-			position: new google.maps.LatLng(pinLocations[i].lat, pinLocations[i].lng),
-			map: map
-		});
+// 	for (i = 0; i < pinLocations.length; i++) {
+// 		markers[i] = new google.maps.Marker({
+// 			position: new google.maps.LatLng(pinLocations[i].lat, pinLocations[i].lng),
+// 			map: map
+// 		});
 
-		markers[i].index = i;
-		contents[i] = '<div class="popup_container">'+'<b>'+pinLocations[i].name+'</b>'+'</div>';
+// 		markers[i].index = i;
+// 		contents[i] = '<div class="popup_container">'+'<b>'+pinLocations[i].name+'</b>'+'</div>';
 
-		infowindows[i] = new google.maps.InfoWindow({
-			content: contents[i],
-			maxWidth: 300
-		});
+// 		infowindows[i] = new google.maps.InfoWindow({
+// 			content: contents[i],
+// 			maxWidth: 300
+// 		});
 
-	//adding labels when you click	
-		google.maps.event.addListener(markers[i], "click", function() {
-			console.log(this.index);
-			infowindows[this.index].open(map,markers[this.index]);
-			map.panTo(markers[this.index].getPosition());
-		});
-	}
+// 	//adding labels when you click	
+// 		google.maps.event.addListener(markers[i], "click", function() {
+// 			console.log(this.index);
+// 			infowindows[this.index].open(map,markers[this.index]);
+// 			map.panTo(markers[this.index].getPosition());
+// 		});
+// 	}
 
-	//center map on where pins are
-	var bounds = markers.reduce(function(bounds, marker) {
-		return bounds.extend(marker.getPosition());
-	}, new google.maps.LatLngBounds());
+// 	//center map on where pins are
+// 	var bounds = markers.reduce(function(bounds, marker) {
+// 		return bounds.extend(marker.getPosition());
+// 	}, new google.maps.LatLngBounds());
 	
-	map.setCenter(bounds.getCenter());
-	map.fitBounds(bounds);
-}
+// 	map.setCenter(bounds.getCenter());
+// 	map.fitBounds(bounds);
+// }
 
 
-function deleteOverlays() {
-	console.log("deleteOverlays");
-	if (markers) {
-		for (i in markers) {
-			markers[i].setMap(null);
-		}
-		markers.length = 0;
-	}
-}
+// function deleteOverlays() {
+// 	console.log("deleteOverlays");
+// 	if (markers) {
+// 		for (i in markers) {
+// 			markers[i].setMap(null);
+// 		}
+// 		markers.length = 0;
+// 	}
+// }
 
 
 
