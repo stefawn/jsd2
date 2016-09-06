37b5f2c482a5e23e6b77b5ba2def6e4c128e2303
Lindsey Sugino <lindseysugino@gmail.com>
2016-09-05 20:28:31 -0700

    reorganize js + html

--- homework/Foodstr/js/foodstr.js
+++ homework/Foodstr/js/foodstr.js
@@ -1,34 +1,47 @@
-// Elements
+// Selectors
+// ------------------------------------
+
 var form = document.querySelector("form");
 var search = document.querySelector("#search");
 var results = document.querySelector(".results");
 var display = document.querySelector(".display");
 var favoritesList = document.querySelector(".favorites-list");
 var clear = document.querySelector(".clear");
-var button;
-
 var headerTemplate = document.querySelector("#header-template");
 var restaurantTemplate = document.querySelector("#restaurant-template");
 
-var map; 
-var markers = [];
-var contents = [];
-var infowindows = [];
-var pinLocations = [];
+// Variables
+// ------------------------------------
+
 var favoriteRestaurants = [];
 
 // Events
 // ------------------------------------
+
 window.addEventListener("load", loadListFirebase);
 form.addEventListener("submit", getRestaurants);
 display.addEventListener("click", saveRestaurant);
 clear.addEventListener("click", clearFavorites);
 
+// Firebase Functions
+// ------------------------------------
+
 // Firebase Setup
 var firebaseRef = new Firebase("https://foodstr-a1343.firebaseio.com/");
 
-// Event Handlers 
+function loadListFirebase() {
+	console.log('listFirebase');
+	firebaseRef.on("value", loadRestaurants);
+}
+
+function favListFirebase() {
+	console.log('favlistFirebase');
+	firebaseRef.set(favoriteRestaurants);
+}
+
+// Restaurant Functions
 // ------------------------------------
+
 function getRestaurants(event) {
 	event.preventDefault();
 	var searchString = search.value;
@@ -74,9 +87,6 @@ function clearFavorites() {
 	favListFirebase();
 }
 
-
-// Update page
-// ------------------------------------
 function loadRestaurants(snapshot) {
 	console.log('loadRestaurants...');
 	console.log(snapshot.val());
@@ -93,6 +103,9 @@ function loadRestaurants(snapshot) {
 	}
 }
 
+// Update page
+// ------------------------------------
+
 function populateFavs(restaurantName) { 
 	console.log('populateFavs...');
 	
@@ -102,7 +115,6 @@ function populateFavs(restaurantName) {
 }
 
 function updateRestaurants(json) {
-	
 	clearPrevious();
 	// compile results template
 	var template = Handlebars.compile(headerTemplate.innerHTML);
@@ -114,98 +126,4 @@ function updateRestaurants(json) {
 	json.restaurants.forEach(savePinLocations);
 
 	createMarkers();
-
 }
-
-// Firebase Functions
-// ------------------------------------
-function loadListFirebase() {
-	console.log('listFirebase');
-	firebaseRef.on("value", loadRestaurants);
-}
-
-function favListFirebase() {
-	console.log('favlistFirebase');
-	firebaseRef.set(favoriteRestaurants);
-}
-
-// Map Functions
-// ------------------------------------
-function initMap() {
-	loadMap();
-}
-
-function loadMap() {
-	var el = document.querySelector('#places-map');
-
-	var options = {
-		center: { lat: 37.798969, lng: -122.407312 },
-		zoom: 10
-	};
-
-	map = new google.maps.Map(el, options);
-
-}
-
-function createMarkers() {
-	// clears out previous map markers
-	deleteOverlays();
-
-	for (i = 0; i < pinLocations.length; i++) {
-		markers[i] = new google.maps.Marker({
-			position: new google.maps.LatLng(pinLocations[i].lat, pinLocations[i].lng),
-			map: map
-		});
-
-		markers[i].index = i;
-		contents[i] = '<div class="popup_container">'+'<b>'+pinLocations[i].name+'</b>'+'</div>';
-
-		infowindows[i] = new google.maps.InfoWindow({
-			content: contents[i],
-			maxWidth: 300
-		});
-
-	//adding labels when you click	
-		google.maps.event.addListener(markers[i], "click", function() {
-			console.log(this.index);
-			infowindows[this.index].open(map,markers[this.index]);
-			map.panTo(markers[this.index].getPosition());
-		});
-	}
-
-	//center map on where pins are
-	var bounds = markers.reduce(function(bounds, marker) {
-		return bounds.extend(marker.getPosition());
-	}, new google.maps.LatLngBounds());
-	
-	map.setCenter(bounds.getCenter());
-	map.fitBounds(bounds);
-}
-
-function deleteOverlays() {
-	console.log("deleteOverlays");
-	if (markers) {
-		for (i in markers) {
-			markers[i].setMap(null);
-		}
-		markers.length = 0;
-	}
-}
-
-function clearPrevious() {
-	// clears out the old results
-	results.innerHTML = '';
-	pinLocations = [];
-}
-
-function savePinLocations(restaurant) {
-	console.log("savePinLocations");
-	pinLocations.push({name: restaurant.name, lat: restaurant.lat, lng: restaurant.lng});
-}
-
-
-// $('div').on('click', function() {
-// 	$(this).toggleClass('show-description');
-// });
-
-
